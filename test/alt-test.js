648bdfd53d22201693aee4771281476d4a231f3c
jesse keane <me@jessekeane.me>
2013-12-04 19:10:26 -0800

    fix tests

--- test/alt-test.js
+++ test/alt-test.js
@@ -1,37 +1,37 @@
 var assert = require('assert'),
     send = require('../'),
-    http = require('http');
+    http = require('http')
 
-http.ServerResponse.prototype.send = send;
+http.ServerResponse.prototype.send = send
 
 var server = http.createServer(function (req, res) {
-  if (req.url == '/first') return res.send(res, 403, { hello: 'world' });
-  if (req.url == '/second') return res.send(res, { hi: 'there' });
-}).listen(96288);
+  if (req.url == '/first') return res.send(res, 403, { hello: 'world' })
+  if (req.url == '/second') return res.send(res, { hi: 'there' })
+}).listen(9628)
 
-http.get({ port: 96288, path: '/first' }, function (res) {
-  var data = '';
-  assert.equal(res.statusCode, 403);
+http.get({ port: 9628, path: '/first' }, function (res) {
+  var data = ''
+  assert.equal(res.statusCode, 403)
   res.on('data', function (chunk) {
-    data += chunk;
-  });
+    data += chunk
+  })
   res.on('end', function () {
     assert.equal(data, '{"hello":"world"}');
-    testWithoutCode();
-  });
-});
+    testWithoutCode()
+  })
+})
 
 function testWithoutCode() {
-  http.get({ port: 96288, path: '/second' }, function (res) {
-    var data = '';
-    assert.equal(res.statusCode, 200);
+  http.get({ port: 9628, path: '/second' }, function (res) {
+    var data = ''
+    assert.equal(res.statusCode, 200)
     res.on('data', function (chunk) {
-      data += chunk;
-    });
+      data += chunk
+    })
     res.on('end', function () {
-      assert.equal(data, '{"hi":"there"}');
-      server.close();
-    });
-  });
+      assert.equal(data, '{"hi":"there"}')
+      server.close()
+    })
+  })
 }
 
--- test/test.js
+++ test/test.js
@@ -1,35 +1,35 @@
 var assert = require('assert'),
     send = require('../'),
-    http = require('http');
+    http = require('http')
 
 var server = http.createServer(function (req, res) {
-  if (req.url == '/first') return send(res, 403, { hello: 'world' });
-  if (req.url == '/second') return send(res, { hi: 'there' });
-}).listen(96288);
+  if (req.url == '/first') return send(res, 403, { hello: 'world' })
+  if (req.url == '/second') return send(res, { hi: 'there' })
+}).listen(9628)
 
-http.get({ port: 96288, path: '/first' }, function (res) {
+http.get({ port: 9628, path: '/first' }, function (res) {
   var data = '';
-  assert.equal(res.statusCode, 403);
+  assert.equal(res.statusCode, 403)
   res.on('data', function (chunk) {
     data += chunk;
-  });
+  })
   res.on('end', function () {
     assert.equal(data, '{"hello":"world"}');
-    testWithoutCode();
-  });
-});
+    testWithoutCode()
+  })
+})
 
 function testWithoutCode() {
-  http.get({ port: 96288, path: '/second' }, function (res) {
-    var data = '';
-    assert.equal(res.statusCode, 200);
+  http.get({ port: 9628, path: '/second' }, function (res) {
+    var data = ''
+    assert.equal(res.statusCode, 200)
     res.on('data', function (chunk) {
-      data += chunk;
-    });
+      data += chunk
+    })
     res.on('end', function () {
-      assert.equal(data, '{"hi":"there"}');
-      server.close();
-    });
-  });
+      assert.equal(data, '{"hi":"there"}')
+      server.close()
+    })
+  })
 }
 
