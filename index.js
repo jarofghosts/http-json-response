03c31498845299f7e37e4ebbdaa7ea0b284e972f
jesse keane <me@jessekeane.me>
2014-07-04 21:15:33 -0700

    update tests, clean up code

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ var http = require('http')
 module.exports = send
 
 function send(res, code, obj) {
-  if (!(res instanceof http.ServerResponse)) {
+  if(this instanceof http.ServerResponse) {
     obj = code
     code = res
     res = this
   }
 
-  if (typeof code === 'object') {
+  if(typeof code !== 'number') {
     obj = code
     code = 200
   }
 
   res.writeHead(code, {'content-type': 'application/json'})
 
+  return write(res, obj)
+}
+
+function write(res, obj) {
   try {
     res.end(JSON.stringify(obj))
     return true
